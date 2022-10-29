e1ee023521899506c6ae98a933a3fdb4ef711ae6
Samuel Sanchez <ssancpas@everis.com>
2022-10-29 16:13:38 +0200

    FEAT: added pages templates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,39 @@ const routes: Routes = [
       keywords: 'Angular, fisioterapia, Pandemia,mascarillas, papel coronavirus, Mapa de España, como hacer mascarillas, españa coronavirus, retos fisiotteriapia, toilet paper challenge'
     }
   },
+  // {
+  //   path: 'about-me',
+  //   // component: AboutMeComponent,
+  //   // loadChildren: () => import('./pages/about-me/about-me.module').then(m => m.AboutMeModule),
+  //   data: {
+  //     title: 'Sobre nosotros',
+  //     description: 'Fisioterapia es una pagina web diseñada para divertir y entretener a la gente. Las mejores e interesantes noticias en cuanto a papel, sobre la casa de papel y además memtes y retos/challenges.',
+  //     ogUrl: 'https://fision.es/about-me',
+  //     keywords: 'about'
+  //   }
+  // },
+  // {
+  //   path: 'contact',
+  //   // component: ContactComponent,
+  //   // loadChildren: () => import('./pages/about-me/about-me.module').then(m => m.AboutMeModule),
+  //   data: {
+  //     title: 'Sobre nosotros',
+  //     description: 'Fisioterapia es una pagina web diseñada para divertir y entretener a la gente. Las mejores e interesantes noticias en cuanto a papel, sobre la casa de papel y además memtes y retos/challenges.',
+  //     ogUrl: 'https://fision.es/about-me',
+  //     keywords: 'about'
+  //   }
+  // },
+  {
+    path: 'prices',
+    // component: HomeComponent,
+    loadChildren: () => import('./pages/prices/prices.module').then(m => m.PricesModule),
+    data: {
+      title: 'Portfolio Josefina',
+      description: 'Página web relacionada con el fisioterapia, mapa con el consumo de fisioterapia en España. Memes y mucho más. Entra y pasa un rato divertido!',
+      ogUrl: 'https://papelpandemia.es',
+      keywords: 'Angular, fisioterapia, Pandemia,mascarillas, papel coronavirus, Mapa de España, como hacer mascarillas, españa coronavirus, retos fisiotteriapia, toilet paper challenge'
+    }
+  },
 
   { path: '**', redirectTo: 'home' },
 
--- src/app/pages/prices/prices.module.ts
+++ src/app/pages/prices/prices.module.ts
@@ -1,29 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AboutMeComponent } from '../about-me/about-me.component';
 import { ContactComponent } from '../contact/contact.component';
-import { PricesComponent } from '../prices/prices.component';
+import { PricesComponent } from './prices.component';
 
 @NgModule({
   declarations: [
-    HomeComponent,
-    AboutMeComponent,
-    ContactComponent,
-    PricesComponent
+    // PricesComponent,
   ],
   imports: [
     CommonModule,
     RouterModule.forChild([
       {
         path: '',
-        component: HomeComponent
+        component: PricesComponent
       }
     ]),
-    SharedModule
   ],
   exports: [RouterModule]
 })
-export class HomeModule { }
+export class PricesModule { }
